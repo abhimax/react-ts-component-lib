9580c14f86c3225cb7de56dfbcc9b87f02f2453c
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-03-30 08:59:24 +0530

    feat: add btn component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./button.css";
+
+interface ButtonProps {
+  primary?: boolean;
+  icon?: HTMLElement;
+  size?: "small" | "medium" | "large";
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button = ({
+  primary = false,
+  size = "medium",
+  icon,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      {...props}
+    >
+      <>
+        {icon}
+        {label}
+      </>
+    </button>
+  );
+};

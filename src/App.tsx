272b09160123ccb2e38f75809b9bd835d804fb8e
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-04-03 00:34:25 +0530

    feat: add icon component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 //import "./App.scss";
 import Button from "./components/Button/Button";
+import Icon from "./components/Icon/Icon";
 
 function App() {
   return (
     <div className="App">
       <Button label="Test UI" />
+      <Icon name="cross" size={24} />
     </div>
   );
 }

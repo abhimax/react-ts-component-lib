acb73c0d10e924e271ac12e19caf78744ed156db
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-04-08 16:04:40 +0530

    feat: star rating stories

---
+++ src/components/StarRating/StarRating.stories.tsx
@@ -0,0 +1,79 @@
+// import React from "react";
+// import StarRating from "./StarRating";
+
+// export default {
+//   title: "UI/StarRating",
+//   component: StarRating,
+// };
+// const Template = (args) => <StarRating {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {};
+// export const Rating2 = Template.bind({});
+// Rating2.args = {
+//   rating: 2,
+// };
+// export const Rating5 = Template.bind({});
+// Rating5.args = {
+//   rating: 5,
+// };
+// export const Rating10 = Template.bind({});
+// Rating10.args = {
+//   rating: 10,
+// };
+// export const Rating3point2 = Template.bind({});
+// Rating3point2.args = {
+//   rating: 3.2,
+// };
+// export const Rating3point5 = Template.bind({});
+// Rating3point5.args = {
+//   rating: 3.5,
+// };
+
+// export const Rating3point8 = Template.bind({});
+// Rating3point8.args = {
+//   rating: 3.8,
+//   fillPercentage: 50,
+// };
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import StarRating from "./StarRating";
+
+export default {
+  title: "Components/StarRating",
+  component: StarRating,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof StarRating>;
+
+const Template: ComponentStory<typeof StarRating> = (args) => (
+  <StarRating {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  rating: 2,
+};
+export const Rating5 = Template.bind({});
+Rating5.args = {
+  rating: 5,
+};
+export const Rating10 = Template.bind({});
+Rating10.args = {
+  rating: 10,
+};
+export const Rating3point2 = Template.bind({});
+Rating3point2.args = {
+  rating: 3.2,
+};
+export const Rating3point5 = Template.bind({});
+Rating3point5.args = {
+  rating: 3.5,
+};
+
+export const Rating3point8 = Template.bind({});
+Rating3point8.args = {
+  rating: 3.8,
+};

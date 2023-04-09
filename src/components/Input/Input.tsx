4fccd595edf26da05071b1805c13ba2a4a32e61d
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-04-10 02:42:08 +0530

    feat: input component cleanups and styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,38 @@
-import React, { InputHTMLAttributes } from "react";
+import React from "react";
+import classNames from "classnames";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+type InputProps = {
   label: string;
+  size?: "small" | "medium" | "large";
   error?: string;
-}
+} & Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "size" | "label" | "error"
+>;
 
-const Input: React.FC<InputProps> = ({ label, error, ...props }) => {
-  const inputClassName = error ? "input error" : "input";
+const Input: React.FC<InputProps> = ({
+  label,
+  size = "medium",
+  error,
+  className,
+  ...props
+}) => {
+  const inputClassName = classNames(
+    "input",
+    {
+      "input--small": size === "small",
+      "input--medium": size === "medium",
+      "input--large": size === "large",
+      "input--error": !!error,
+    },
+    className
+  );
+
   return (
-    <div>
-      <label>{label}</label>
-      <input {...props} />
+    <div className={`form-group ${inputClassName}`}>
+      <label className="input-label">{label}</label>
       {error && <div className="error-message">{error}</div>}
+      <input className="form-input" {...props} />
     </div>
   );
 };

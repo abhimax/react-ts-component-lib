744cf1f78896648193f915de21fcb6cd27c835d7
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-04-13 00:03:30 +0530

    feat: input  imp to support it's default types

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import classNames from "classnames";
 
 type InputProps = {
-  label: string;
+  label?: string;
   size?: "small" | "medium" | "large";
   error?: string;
   className?: string;
+  type?: string;
 } & Omit<
   React.InputHTMLAttributes<HTMLInputElement>,
-  "size" | "label" | "error" | "className"
+  "size" | "label" | "error" | "className" | "type"
 >;
 
 const Input: React.FC<InputProps> = ({
@@ -16,20 +17,22 @@ const Input: React.FC<InputProps> = ({
   size = "medium",
   error,
   className,
+  type,
   ...props
 }) => {
   const inputClassName = classNames(
-    "input",
+    "form-group",
     size ? `input--${size}` : "",
-    error ? `input--${error}` : "",
-    className
+    error ? `input--error` : "",
+    className,
+    type ? `input--${type}` : ""
   );
 
   return (
-    <div className={`form-group ${inputClassName}`}>
+    <div className={inputClassName}>
       <label className="input-label">{label}</label>
+      <input className="form-input" type={type} {...props} />
       {error && <div className="error-message">{error}</div>}
-      <input className="form-input" {...props} />
     </div>
   );
 };

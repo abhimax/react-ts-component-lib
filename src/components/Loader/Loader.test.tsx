a2233745a4ae9d1fc3a8fdd6b8acdb8bee4d0592
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-06-23 01:06:21 +0530

    feat: updated loader unit tests

--- src/components/Loader/Loader.test.tsx
+++ src/components/Loader/Loader.test.tsx
@@ -1,33 +1,75 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Loader from "./Loader";
-import TestRenderer from "react-test-renderer";
 
-describe("Loader", () => {
-  it("renders Loader", () => {
-    const { container } = render(<Loader />);
-    expect(container.firstChild).toHaveClass("loader-container");
-    expect(container.firstChild?.firstChild).toHaveClass("loader-circle");
-  });
+describe("Loader component", () => {
+  it("renders the loader with the provided message and size", () => {
+    const message = "Loading...";
+    const size = "50px";
+    render(<Loader message={message} size={size} />);
 
-  it("renders Loader with different props", () => {
-    const { container } = render(<Loader size="40px" />);
-    expect(container.firstChild).toHaveStyle("width: 40px; height: 40px;");
-    expect(container.firstChild?.firstChild).toHaveStyle(
-      "width: calc(40px - 6px); height: calc(40px - 6px);"
+    const loaderMessage = screen.getByText(message);
+    expect(loaderMessage).toBeInTheDocument();
+
+    const loaderCircle = screen.getByTestId("id-loader-circle");
+    expect(loaderCircle).toHaveStyle(`width: calc(${size} - 6px)`);
+    expect(loaderCircle).toHaveStyle(`height: calc(${size} - 6px)`);
+
+    const loaderMessageContainer = screen.getByText(message).parentElement;
+    expect(loaderMessageContainer).toHaveStyle(
+      `marginTop: calc(${size} * 0.25)`
     );
   });
 
-  it("Loader renders correctly", () => {
-    const tree = TestRenderer.create(<Loader />).toJSON();
-    expect(tree).toMatchSnapshot();
+  it("renders the loader with the default message and size when no message and size are provided", () => {
+    render(<Loader />);
+
+    const defaultLoaderMessage = screen.getByText("Loading...");
+    expect(defaultLoaderMessage).toBeInTheDocument();
+
+    const defaultLoaderCircle = screen.getByTestId("id-loader-circle");
+    expect(defaultLoaderCircle).toHaveStyle(`width: calc(70px - 6px)`);
+    expect(defaultLoaderCircle).toHaveStyle(`height: calc(70px - 6px)`);
+
+    const defaultLoaderMessageContainer =
+      screen.getByText("Loading...").parentElement;
+    expect(defaultLoaderMessageContainer).toHaveStyle(
+      `marginTop: calc(70px * 0.25)`
+    );
   });
 
-  it("Loader styles are applied correctly", () => {
-    const { container } = render(<Loader size="50px" />);
-    expect(container.firstChild).toHaveStyle("width: 50px; height: 50px;");
-    expect(container.firstChild?.firstChild).toHaveStyle(
-      "width: calc(50px - 6px); height: calc(50px - 6px);"
+  it("renders the loader with the provided message and default size when only message is provided", () => {
+    const message = "Please wait...";
+    render(<Loader message={message} />);
+
+    const loaderMessage = screen.getByText(message);
+    expect(loaderMessage).toBeInTheDocument();
+
+    const defaultLoaderCircle = screen.getByTestId("id-loader-circle");
+    expect(defaultLoaderCircle).toHaveStyle(`width: calc(70px - 6px)`);
+    expect(defaultLoaderCircle).toHaveStyle(`height: calc(70px - 6px)`);
+
+    const defaultLoaderMessageContainer =
+      screen.getByText(message).parentElement;
+    expect(defaultLoaderMessageContainer).toHaveStyle(
+      `marginTop: calc(70px * 0.25)`
+    );
+  });
+
+  it("renders the loader with the default message and the provided size when only size is provided", () => {
+    const size = "100px";
+    render(<Loader size={size} />);
+
+    const defaultLoaderMessage = screen.getByText("Loading...");
+    expect(defaultLoaderMessage).toBeInTheDocument();
+
+    const loaderCircle = screen.getByTestId("id-loader-circle");
+    expect(loaderCircle).toHaveStyle(`width: calc(${size} - 6px)`);
+    expect(loaderCircle).toHaveStyle(`height: calc(${size} - 6px)`);
+
+    const defaultLoaderMessageContainer =
+      screen.getByText("Loading...").parentElement;
+    expect(defaultLoaderMessageContainer).toHaveStyle(
+      `marginTop: calc(${size} * 0.25)`
     );
   });
 });
